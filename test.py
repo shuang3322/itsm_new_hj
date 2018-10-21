fec45d54f94e52c6653979a8debd7af819d81826
shuang3322 <shuang0528@hotmail.com>
2018-10-21 18:22:02 +0800

    run ping test

--- test.py
+++ test.py
@@ -3,17 +3,35 @@
 # coding:utf-8
 
 import os
+import subprocess
+import time
+from multiprocessing import Pool
 
-v = ('10.78.40.%d' % (x) for x in range(170, 176))
 
-for it in v:
-
-    ret = os.system('ping  %s -n 1 &> /dev/null' % it)
-    print('ping -c 1 -W 1 %s &> /dev/null' % it)
-    if ret:
-
-        print('ping %s fail' % it)
-
+def ping_call(num):
+    fnull = open(os.devnull, 'w')
+    ipaddr = 'ping 10.77.40.' + str(num)
+    # result = subprocess.call(ipaddr + ' -c 1', shell=True, stdout=fnull, stderr=fnull)
+    result = subprocess.call(ipaddr + ' -n 1', shell=True, stdout=fnull, stderr=fnull,)
+    current_time = time.strftime('%Y%m%d-%H:%M:%S', time.localtime())
+    print(result)
+    if result:
+        pass
+        # print('时间:{} ip地址:{} ping fall'.format(current_time, ipaddr))
     else:
+        print('时间:{} ip地址:{} ping ok'.format(current_time, ipaddr))
 
-        print('ping %s ok' % it)
+    fnull.close()
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    p = Pool(20)
+    res_l = []
+    for i in range(1, 20):
+        res = p.apply_async(ping_call, args=(i,))
+        res_l.append(res)
+    for res in res_l:
+        res.get()
+
+    print('程序耗时{:.2f}'.format(time.time() - start_time))
